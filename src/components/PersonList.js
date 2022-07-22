dd590d93c45208c7e306990193088db3234b9ee4
mattjkatz <matthewkatz7@gmail.com>
2022-07-21 20:29:17 -0500

    axios call works

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -23,3 +23,13 @@ export default class PersonList extends React.Component {
     );
   }
 }
+
+// export default function PersonList() {
+//   return (
+//     <nav>
+//       <img src={require("../images/react-icon-small.png")} alt="" className="nav--icon" />
+//       <h3 className="nav--logo_text">ReactFacts</h3>
+//       <h4 className="nav--title">React Course - Project 1</h4>
+//     </nav>
+//   );
+// }
